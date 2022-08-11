16c6c426b3c9ff27f747667c7733c21f289aaad3
Artem Tarasov <70967111+VaeSemper@users.noreply.github.com>
2022-08-11 12:41:50 +0300

    completed f

--- python/sprint1_nonfinals/f.py
+++ python/sprint1_nonfinals/f.py
@@ -1,5 +1,15 @@
-def is_palindrome(line: str) -> bool:
-    # Здесь реализация вашего решения
-    pass
+"""
+Помогите Васе понять, будет ли фраза палиндромом. Учитываются только буквы
+и цифры, заглавные и строчные буквы считаются одинаковыми.
 
-print(is_palindrome(input().strip()))
+Решение должно работать за O(N), где N — длина строки на входе.
+"""
+
+
+def is_palindrome(line: str) -> bool:
+    line = ''.join(e for e in line if e.isalnum()).lower()
+    return line == line[::-1]
+
+
+if __name__ == '__main__':
+    print(is_palindrome(input().strip()))
