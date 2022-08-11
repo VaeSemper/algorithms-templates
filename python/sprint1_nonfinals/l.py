a6445b2de8439fa25a879a5150efc5f601a49c72
Artem Tarasov <70967111+VaeSemper@users.noreply.github.com>
2022-08-11 17:19:10 +0300

    completed l

--- python/sprint1_nonfinals/l.py
+++ python/sprint1_nonfinals/l.py
@@ -1,13 +1,25 @@
+"""
+Васе очень нравятся задачи про строки, поэтому он придумал свою. Есть 2
+строки s и t, состоящие только из строчных букв. Строка t получена
+перемешиванием букв строки s и добавлением 1 буквы в случайную позицию.
+Нужно найти добавленную букву.
+"""
 from typing import Tuple
 
+
 def get_excessive_letter(shorter: str, longer: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+    result = list(longer)
+    for char in shorter:
+        result.remove(char)
+    return result[0]
+
 
 def read_input() -> Tuple[str, str]:
     shorter = input().strip()
     longer = input().strip()
     return shorter, longer
 
-shorter, longer = read_input()
-print(get_excessive_letter(shorter, longer))
+
+if __name__ == '__main__':
+    shorter, longer = read_input()
+    print(get_excessive_letter(shorter, longer))
