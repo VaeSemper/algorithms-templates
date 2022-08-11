eb3e38157fb41d5027ebabf4510ec4bb6367c245
Artem Tarasov <70967111+VaeSemper@users.noreply.github.com>
2022-08-11 12:32:40 +0300

    completed e

--- python/sprint1_nonfinals/e.py
+++ python/sprint1_nonfinals/e.py
@@ -1,14 +1,33 @@
-def get_longest_word(line: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+"""
+Чтобы подготовиться к семинару, Гоше надо прочитать статью по
+эффективному менеджменту. Так как Гоша хочет спланировать день заранее,
+ему необходимо оценить сложность статьи.
 
-def read_input() -> str:
+Он придумал такой метод оценки: берётся случайное предложение из текста и в
+нём ищется самое длинное слово. Его длина и будет условной сложностью
+статьи.
+"""
+from typing import List
+
+
+def get_longest_word(line: List[str]) -> str:
+    result = ''
+    for word in line:
+        if len(word) > len(result):
+            result = word
+    return result
+
+
+def read_input() -> List[str]:
     _ = input()
-    line = input().strip()
+    line = input().strip().split()
     return line
+
 
 def print_result(result: str) -> None:
     print(result)
     print(len(result))
 
-print_result(get_longest_word(read_input()))
+
+if __name__ == '__main__':
+    print_result(get_longest_word(read_input()))
