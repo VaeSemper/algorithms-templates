d907e56e3237d7347b914f3b8e944fd9276256ef
Artem Tarasov <70967111+VaeSemper@users.noreply.github.com>
2022-08-11 12:56:38 +0300

    completed g

--- python/sprint1_nonfinals/g.py
+++ python/sprint1_nonfinals/g.py
@@ -1,8 +1,25 @@
+"""
+Вася реализовал функцию, которая переводит целое число из десятичной системы
+в двоичную. Но, кажется, она получилась не очень оптимальной.
+
+Попробуйте написать более эффективную программу.
+"""
+
+
 def to_binary(number: int) -> str:
-    # Здесь реализация вашего решения
-    pass
+    binary = ''
+    while number:
+        if number & 1:
+            binary += '1'
+        else:
+            binary += '0'
+        number >>= 1
+    return binary[::-1]
+
 
 def read_input() -> int:
     return int(input().strip())
 
-print(to_binary(read_input()))
+
+if __name__ == '__main__':
+    print(to_binary(read_input()))
