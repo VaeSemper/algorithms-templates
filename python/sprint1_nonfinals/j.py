7d8bfe6deff2523777a8b51d3746ec1f35e57113
Artem Tarasov <70967111+VaeSemper@users.noreply.github.com>
2022-08-11 15:39:16 +0300

    completed j

--- python/sprint1_nonfinals/j.py
+++ python/sprint1_nonfinals/j.py
@@ -1,8 +1,41 @@
+from math import sqrt
 from typing import List
 
-def factorize(number: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
 
-result = factorize(int(input()))
-print(" ".join(map(str, result)))
+# def factorize(number: int) -> List[int]:
+#     while number > 1:
+#         for i in range(2, number + 1):
+#             if number % i == 0:
+#                 number //= i
+#                 yield i
+#                 break
+
+
+
+# def factorize(number: int) -> List[int]:
+#     result = []
+#     for i in range(2, number + 1):
+#         while number % i == 0:
+#             result.append(i)
+#             # print(i, end=' ')
+#             number //= i
+#     return result
+
+
+def factorize(number: int) -> List[int]:
+    j = 2
+    while number > 1:
+        for i in range(j, int(sqrt(number+0.05)) + 1):
+            if number % i == 0:
+                number /= i
+                j = i
+                yield i
+                break
+        else:
+            if number > 1:
+                yield number
+                break
+
+
+if __name__ == '__main__':
+    print(' '.join(map(str, factorize(int(input())))).rstrip('.0'))
