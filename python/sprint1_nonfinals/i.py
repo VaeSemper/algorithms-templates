49ad55ed3f6b1e207da1f145832d83fdc45cf050
Artem Tarasov <70967111+VaeSemper@users.noreply.github.com>
2022-08-11 14:19:28 +0300

    completed i

--- python/sprint1_nonfinals/i.py
+++ python/sprint1_nonfinals/i.py
@@ -1,5 +1,16 @@
-def is_power_of_four(number: int) -> bool:
-    # Здесь реализация вашего решения
-    pass
+"""
+Напишите программу, которая определяет, будет ли положительное целое число
+степенью четвёрки.
 
-print(is_power_of_four(int(input())))
+Подсказка: степенью четвёрки будут все числа вида 4n, где n – целое
+неотрицательное число.
+"""
+from math import log
+
+
+def is_power_of_four(number: int) -> bool:
+    return log(number, 4).is_integer()
+
+
+if __name__ == '__main__':
+    print(is_power_of_four(int(input())))
