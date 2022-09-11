554f51e38ec1b0abb3601cab977c9c26c4beb515
Artem Tarasov <70967111+VaeSemper@users.noreply.github.com>
2022-09-11 13:03:37 +0300

    completed C

--- python/sprint2/C/code.py
+++ python/sprint2/C/code.py
@@ -1,5 +1,14 @@
 # ! change LOCAL to False before submitting !
 # set LOCAL to True for local testing
+"""
+Вася размышляет, что ему можно не делать из того списка дел, который он
+составил. Но, кажется, все пункты очень важные! Вася решает загадать число и
+удалить дело, которое идёт под этим номером. Список дел представлен в виде
+односвязного списка. Напишите функцию solution, которая принимает на вход
+голову списка и номер удаляемого дела и возвращает голову обновлённого
+списка.
+"""
+
 
 LOCAL = True
 
@@ -11,21 +20,32 @@ if LOCAL:
 
 
 def solution(node, idx):
-    # Your code
-    # ヽ(´▽`)/
-    pass
+    def get_node_by_index(node, index):
+        while index:
+            node = node.next_item
+            index -= 1
+        return node
+    if idx == 0:
+        node = node.next_item
+    else:
+        previous_node = get_node_by_index(node, idx - 1)
+        next_node = get_node_by_index(node, idx + 1)
+        previous_node.next_item = next_node
+    return node
+
 
 def test():
-    node3 = Node("node3", None)
-    node2 = Node("node2", node3)
-    node1 = Node("node1", node2)
-    node0 = Node("node0", node1)
+    node3 = Node('node3', None)
+    node2 = Node('node2', node3)
+    node1 = Node('node1', node2)
+    node0 = Node('node0', node1)
     new_head = solution(node0, 1)
     assert new_head is node0
     assert new_head.next_item is node2
     assert new_head.next_item.next_item is node3
     assert new_head.next_item.next_item.next_item is None
     # result is node0 -> node2 -> node3
+
 
 if __name__ == '__main__':
     test()
